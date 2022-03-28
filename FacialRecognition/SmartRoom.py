b64106d195e27176fb2290a814afd705ee3e9de5
wmohammedw <69542048+wmohammedw@users.noreply.github.com>
2022-03-28 17:19:03 +0300

    add methods and comments

---
+++ FacialRecognition/SmartRoom.py
@@ -0,0 +1,68 @@
+
+class SmartRoom:
+
+    '''
+        this is not the final version. we may add new methods if we need it. 
+    '''
+
+    def __init__(self):
+        '''
+            You need to initilize the variables here before use them in the below methods.
+            follow the OOP concepts.
+
+            for example in model_training method:
+                you will initilize the batch_size here:
+                batch_size = 32
+
+                and use it inside model_training method.
+        '''
+        pass
+
+    def extract_faces(self):
+        '''
+            here you neet to write a function to extract the faces inide each image. you may use
+            haar cascade or MTCNN (better) algorithms.
+
+            you can add as many parameters you want.
+        '''
+        pass
+
+    def data_augmentation(self):
+        '''
+            here you will apply the data augmentation on training set only.
+
+            you can add as many parameters you want.
+        '''
+        pass
+
+    def model_architecture(self):
+        '''
+            here you need to write the model (we will try CNN first with Transfer learning)
+            architecture without compile and fit functions.
+
+            you can add as many parameters you want.
+        '''
+        pass
+
+    def model_training(self):
+        '''
+            here you will set the model configuration such as checkpoint, early stopping, optimizer, 
+            compile, and fit methods.
+
+            you can add as many parameters you want.
+        '''
+        pass
+
+    def arduino(self):
+        '''
+            we will disscused later.
+        '''
+        pass
+
+    def bounded_box(self):
+        '''
+            here you want to show the bounded box around the face.
+
+            you can add as many parameters you want.
+        '''
+        pass
---
+++ FeatureMatching/FakeLogoDetection.py
@@ -0,0 +1,55 @@
+
+class FakeLogoDetection:
+
+    '''
+        this is not the final version. we may add new methods if we need it. 
+    '''
+
+    def __init__(self):
+        '''
+            You need to initilize the variables here before use them in the below methods.
+            follow the OOP concepts.
+
+            for example in load_image method:
+                if you want to resize the image
+                you will define the variable here
+                img_size = 250
+
+                and use it inside load_image method.
+        '''
+        pass
+
+    def load_image(self):
+        '''
+            here you will write a code to load an image from the database (Folder).
+            Also, you will fine the descriptors and keypoints for the laded image and return them.
+
+            you can add as many parameters as you want.
+        '''
+        pass
+
+    def compute_matches(self):
+        '''
+            here you will use either knn or brute force to compute the matching between images.
+        '''
+        pass
+
+    def input_image(self):
+        '''
+            here you will write a code to let the user upload an image from his/her computer.
+        '''
+        pass
+
+    def real_time_matching(self):
+        '''
+            here you need to write a code to open the camera and start matching the image that we 
+            have in the database and start matching.
+        '''
+        pass
+
+    def image_to_image_matching(self):
+        '''
+            in the previous method, we apply it in real time. Now, we need to compare or matching 
+            between two images only.
+        '''
+        pass
