51f3244f84360397a1a4c6610066e8a7171312bc
wmohammedw <69542048+wmohammedw@users.noreply.github.com>
2022-04-08 20:33:45 +0300

    test

--- DenseMotionEstimation/ObjectMovmentPrediction.py
+++ DenseMotionEstimation/ObjectMovmentPrediction.py
@@ -1,7 +1,8 @@
 
-
+import time
 import cv2
 import numpy as np
+
 
 class ObjectMovmentDetection:
     # see the following github https://github.com/niconielsen32/ComputerVision/blob/master/opticalFlow/denseOpticalFlow.py
@@ -10,10 +11,10 @@ class ObjectMovmentDetection:
         pass
 
     def draw_flow(img, flow, step=16):  # omran
-        
         h, w = img.shape[:2]
-        y, x = np.mgrid[step/2:h:step, step/2:w:step].reshape(2,-1).astype(int)
-        fx, fy = flow[y,x].T
+        y, x = np.mgrid[step/2:h:step, step /
+                        2:w:step].reshape(2, -1).astype(int)
+        fx, fy = flow[y, x].T
 
         lines = np.vstack([x, y, x-fx, y-fy]).T.reshape(-1, 2, 2)
         lines = np.int32(lines + 0.5)
@@ -28,18 +29,85 @@ class ObjectMovmentDetection:
 
     def draw_hsv(flow):  # fsfs
         h, w = flow.shape[:2]
-        fx, fy = flow[:,:,0], flow[:,:,1]
-        
+        fx, fy = flow[:, :, 0], flow[:, :, 1]
+
         ang = np.arctan2(fy, fx) + np.pi
         v = np.sqrt(fx*fx+fy*fy)
-        
+
         hsv = np.zeros((h, w, 3), np.uint8)
         hsv[..., 0] = ang*(180/np.pi/2)
         hsv[..., 1] = 255
         hsv[..., 2] = np.minimum(v*4, 255)
         bgr = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
-        
-        return bgr
 
-    def video_motion_estimation(self):  # mhm
-        pass
+        return bgr
+    # output_file_path
+
+    def video_motion_estimation(self, video_file_path):  # mhm
+        video_reader = cv2.VideoCapture(video_file_path)
+
+        # original_video_width = int(video_reader.get(cv2.CAP_PROP_FRAME_WIDTH))
+        # original_video_height = int(
+        #     video_reader.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        # video_writer = cv2.VideoWriter(output_file_path, cv2.VideoWriter_fourcc(
+        #     'M', 'P', '4', 'V'), 24, (original_video_width, original_video_height))
+
+        status, prev = video_reader.read()
+        prevgray = cv2.cvtColor(prev, cv2.COLOR_BGR2GRAY)
+
+        while True:
+            status, frame = video_reader.read()
+
+            if not status:
+                break
+
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            flow = cv2.calcOpticalFlowFarneback(
+                prevgray, gray, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+
+            prevgray = gray
+            cv2.imshow('flow', self.draw_flow(frame, flow))
+            # cv2.imshow('flow HSV', self.draw_hsv(flow))
+
+            key = cv2.waitKey(5)
+            if key == ord('q'):
+                break
+
+            video_reader.release()
+            # video_writer.release()
+
+    def real_time():
+        cap = cv2.VideoCapture(0)
+
+        suc, prev = cap.read()
+        prevgray = cv2.cvtColor(prev, cv2.COLOR_BGR2GRAY)
+
+        while True:
+
+            suc, img = cap.read()
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+            # start time to calculate FPS
+            start = time.time()
+
+            flow = cv2.calcOpticalFlowFarneback(
+                prevgray, gray, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+
+            prevgray = gray
+
+            # End time
+            end = time.time()
+            # calculate the FPS for current frame detection
+            fps = 1 / (end-start)
+
+            print(f"{fps:.2f} FPS")
+
+            cv2.imshow('flow', self.draw_flow(gray, flow))
+            cv2.imshow('flow HSV', self.draw_hsv(flow))
+
+            key = cv2.waitKey(5)
+            if key == ord('q'):
+                break
+
+        cap.release()
+        cv2.destroyAllWindows()
--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 
 from FeatureMatching.FakeLogoDetection import FakeLogosDetection
+from DenseMotionEstimation.ObjectMovmentPrediction import ObjectMovmentDetection
 
 if __name__ == '__main__':
-    fake_logo_detection = FakeLogosDetection()
+    # fake_logo_detection = FakeLogosDetection()
 
     # img_kp1, img_kp2 = fake_logo_detection.load_image(img1_path=r'D:\IAU\5 - year\Second semester\Computer vision\System\FeatureMatching\Dataset\starbox-real.jpg',
     #                                                   img2_path=r'D:\IAU\5 - year\Second semester\Computer vision\System\FeatureMatching\fake logos\starbox-fake.jpg')
@@ -15,7 +16,13 @@ if __name__ == '__main__':
     # fake_logo_detection.image_to_image_matching(
     #     img_kp1[0], img_kp1[1], img_kp2[0], img_kp2[1], matching, 20)
 
-    input_image, input_keypoints, input_descriptors = fake_logo_detection.load_image(
-        img1_path=r'D:\IAU\5 - year\Second semester\Computer vision\System\FeatureMatching\Dataset\bag.jpeg')
-    fake_logo_detection.real_time_matching(
-        input_image, input_keypoints, input_descriptors, MIN_MATCHES=20)
+    # input_image, input_keypoints, input_descriptors = fake_logo_detection.load_image(
+    #     img1_path=r'D:\IAU\5 - year\Second semester\Computer vision\System\FeatureMatching\Dataset\bag.jpeg')
+    # fake_logo_detection.real_time_matching(
+    #     input_image, input_keypoints, input_descriptors, MIN_MATCHES=20)
+
+    denseMotion = ObjectMovmentDetection()
+    # denseMotion.video_motion_estimation(
+    #     video_file_path=r'DenseMotionEstimation\Teens Involved in Car Crash That Was Like a Movie Scene.mp4')
+
+    denseMotion.real_time()
